d52b8552f3305461dc14c39d86ad6196f8eef9b0
Vitalii <mlineckiyvitaliy@gmail.com>
2025-11-24 00:01:26 +0200

    add types chrome

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,7 +1,13 @@
+/// <reference types="chrome" />
 /// <reference types="svelte" />
 /// <reference types="vite/client" />
 
 declare module '*.json' {
   const value: any;
   export default value;
+}
+
+declare module '*?inline' {
+  const content: string;
+  export default content;
 }
